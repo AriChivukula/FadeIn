15963e0e89c5dd23785b12a8b889f57c11ba5d8f
Ari Chivukula <ari@chivuku.la>
2018-07-27 11:48:53 -0700

    Update index.ts

--- static/index.ts
+++ static/index.ts
@@ -8,12 +8,12 @@ window.onload = (): void => {
   root.insertAdjacentHTML("beforeend", `<br />`);
   DB.vertices.filter((vert: any) => vert.label === "VL-scene").forEach(
     (vert: any) => {
-      const lMatches = DB.vertices.filter(vert => vert.label === "VL-location").filter(vert => vert.id === vert.properties.location);
+      const lMatches = DB.vertices.filter((vert: any) => vert.label === "VL-location").filter(vert => vert.id === vert.properties.location);
       root.insertAdjacentHTML("beforeend", `<div class="located location-name" title="${lMatches[0].properties.description}">${vert.properties.exposure}. ${vert.properties.location} - ${vert.properties.time}</div>`);
       let next_is_spoken: boolean = false;
       vert.properties.lines.forEach(
         (line: string) => {
-          const cMatches = DB.vertices.filter(vert => vert.label === "VL-character").filter(vert => vert.id === line);
+          const cMatches = DB.vertices.filter((vert: any) => vert.label === "VL-character").filter(vert => vert.id === line);
           if (cMatches.length > 0) {
             root.insertAdjacentHTML("beforeend", `<div class="speaker character-name" title="${cMatches[0].properties.description}">${line}</div>`);
             next_is_spoken = true;
