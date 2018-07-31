2e4f1b0b7ac05df7bee1f407dffba0d7c4b7fb43
Ari Chivukula <ari@chivuku.la>
2018-07-31 09:56:01 -0400

    Update index.ts

--- static/index.ts
+++ static/index.ts
@@ -6,16 +6,16 @@ window.onload = (): void => {
   root.insertAdjacentHTML("beforeend", `<div class="title">The Sword</div>`);
   root.insertAdjacentHTML("beforeend", `<div class="author">Ari Chivukula</div>`);
   root.insertAdjacentHTML("beforeend", `<br />`);
-  DB.vertices.filter((vert: any) => vert.label === "VL-scene").forEach(
+  DB.vertices.filter((vert: any) => vert.label === "scene").forEach(
     (vert: any) => {
-      const lMatches = DB.vertices.filter((vert: any) => vert.label === "VL-location").filter((vert: any) => vert.id === vert.properties.location);
-      root.insertAdjacentHTML("beforeend", `<div class="located location-name" title="${lMatches[0].properties.description}">${vert.properties.exposure}. ${vert.properties.location} - ${vert.properties.time}</div>`);
+      const lMatches = DB.vertices.filter((vert: any) => vert.label === "location").filter((vert: any) => vert.id === vert.properties["location"].id);
+      root.insertAdjacentHTML("beforeend", `<div class="located location-name" title="${lMatches[0].properties["description"].id}">${vert.properties["exposure"].id}. ${vert.properties["location"].id} - ${vert.properties["time"].id}</div>`);
       let next_is_spoken: boolean = false;
-      vert.properties.lines.forEach(
+      vert.properties["lines"].id.forEach(
         (line: string) => {
-          const cMatches = DB.vertices.filter((vert: any) => vert.label === "VL-character").filter((vert: any) => vert.id === line);
+          const cMatches = DB.vertices.filter((vert: any) => vert.label === "character").filter((vert: any) => vert.id === line);
           if (cMatches.length > 0) {
-            root.insertAdjacentHTML("beforeend", `<div class="speaker character-name" title="${cMatches[0].properties.description}">${line}</div>`);
+            root.insertAdjacentHTML("beforeend", `<div class="speaker character-name" title="${cMatches[0].properties["description"].id}">${line}</div>`);
             next_is_spoken = true;
           } else if (next_is_spoken) {
             root.insertAdjacentHTML("beforeend", `<div class="spoken">${line}</div>`);
